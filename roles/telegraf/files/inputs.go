2ed66052ee7a8feea436ca0dbec23415be7121f1
Lucas Savva <lucas@m1cr0man.com>
2018-10-12 20:54:30 +0100

    Standardise deployment of TIC stack
    - Fix some problems with rsyncing files
    - Update configs for new features in smartos plugin
    - Add syslog input to Telegraf

--- roles/telegraf/files/inputs.go
+++ roles/telegraf/files/inputs.go
@@ -1,11 +1,16 @@
 package all
 
 import (
-        _ "github.com/influxdata/telegraf/plugins/inputs/cpu"
-        _ "github.com/influxdata/telegraf/plugins/inputs/influxdb"
-        _ "github.com/influxdata/telegraf/plugins/inputs/minecraft"
-        _ "github.com/influxdata/telegraf/plugins/inputs/mongodb"
-        _ "github.com/influxdata/telegraf/plugins/inputs/mysql"
-        _ "github.com/influxdata/telegraf/plugins/inputs/nginx"
-        _ "github.com/influxdata/telegraf/plugins/inputs/teamspeak"
+	_ "github.com/influxdata/telegraf/plugins/inputs/cpu"
+	_ "github.com/influxdata/telegraf/plugins/inputs/influxdb"
+	_ "github.com/influxdata/telegraf/plugins/inputs/minecraft"
+	_ "github.com/influxdata/telegraf/plugins/inputs/mongodb"
+	_ "github.com/influxdata/telegraf/plugins/inputs/mysql"
+	_ "github.com/influxdata/telegraf/plugins/inputs/nginx"
+	_ "github.com/influxdata/telegraf/plugins/inputs/syslog"
+	_ "github.com/influxdata/telegraf/plugins/inputs/teamspeak"
+	_ "github.com/m1cr0man/smartos-telegraf-plugins/inputs/smartos_cpu"
+	_ "github.com/m1cr0man/smartos-telegraf-plugins/inputs/smartos_disk"
+	_ "github.com/m1cr0man/smartos-telegraf-plugins/inputs/smartos_net"
+	_ "github.com/m1cr0man/smartos-telegraf-plugins/inputs/smartos_ram"
 )
